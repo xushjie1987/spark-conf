955ba44ec60ceef25b3f1d9cb7e94dff62c2323f
xushjie1987 <xsj2013@foxmail.com>
2016-08-06 23:20:00 +0800

    samsung

---
+++ cluster_1/xbin/clear.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+rm -rf /data/spark-1.6.2/cluster_1/data/*
+rm -rf /data/spark-1.6.2/cluster_1/logs/*
+rm -rf /data/spark-1.6.2/cluster_1/stat/*
+rm -rf /data/spark-1.6.2/cluster_1/work/*
+
+
